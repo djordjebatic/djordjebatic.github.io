17bb52f850007e4b6fa9df987b462bbbb12c117c
djordjebatic <djordjebatic@gmail.com>
2025-11-14 00:34:15 +0000

    Deploying to gh-pages from @ djordjebatic/djordjebatic.github.io@2ccb00e6f4c2fbca1a6951f9a2f8aa432d4fd4f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/assets/pdf/CV_Djordje_Batic.pdf";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-ve-completed-my-phd-work-under-msca-itn-marie-curie-fellowship-and-will-be-starting-a-new-position-as-postdoctoral-researcher-university-of-strathclyde",
+        },{id: "news-i-ve-completed-my-phd-work-under-msca-itn-marie-curie-fellowship-and-will-be-starting-a-new-position-as-postdoctoral-researcher-university-of-strathclyde",
           title: 'I’ve completed my PhD work under MSCA ITN Marie Curie Fellowship and will...',
           description: "",
           section: "News",},{id: "news-our-paper-geodemographic-aware-electric-vehicle-charging-location-planning-for-equitable-placement-using-graph-neural-networks-case-study-of-scotland-metropolitan-areas-energy-got-accepted-in-elsevier-energy",
@@ -43,52 +38,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-ve-open-sourced-my-work-on-carbon-intensity-of-5-million-ev-charging-sessions-in-scotland-check-out-this-github-repo-and-the-associated-huggingface-dataset",
           title: '⚡🔌 I’ve open sourced my work on carbon intensity of 5+ million EV...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
